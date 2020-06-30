e38b2c92890c1039369602b81a2ce2849b48cfbc
vincyvincentv <vincentv@sheridancollege.ca>
2020-06-30 18:41:31 -0400

    Committing the project starter code.

---
+++ src/arithmetic/Arithmetic.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arithmetic;
+
+import java.util.Scanner;
+import static java.time.Clock.system;
+
+/** This class calls the method to perform 
+ * arithmetic operations based on user input
+ * execute the code check the output
+ * @author sivagamasrinivasan
+ * date 30/20
+ */
+public class Arithmetic 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       
+        ArithmeticBase r= new ArithmeticBase();
+        Scanner in= new Scanner(System.in);
+        int n= in.nextInt();
+        int m= in.nextInt();
+        double result = r.calculate(m,n);
+        System.out.println("result :" +result); 
+    
+    }
+}
+
---
+++ src/arithmetic/ArithmeticBase.java
@@ -0,0 +1,42 @@
+package arithmetic;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.Scanner;
+
+/** This class takes String input plus,minus,divide and times
+ * from user and execute the arithmetic operation
+ * change the code to use enum instead String and mention the advantage of enum.
+ * @author sivagamasrinivasan
+ * date 30/20
+ */
+public class ArithmeticBase 
+{
+ public double x,y;
+ public enum VINCYVINCENT_OPERATIONTYPE {PLUS,MINUS,TIMES,DIVIDE};
+    double calculate(double x, double y) 
+        {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter arithmetic operation to Perform: ");
+        String s= sc.next();
+        switch (s.toUpperCase()) 
+        {
+            case "PLUS":
+                return x + y;
+            case "MINUS":
+                return x - y;
+            case "TIMES":
+                return x * y;
+            case "DIVIDE":
+                return x / y;
+            default:
+                throw new AssertionError("Unknown operations " + this);
+        }
+    }
+   
+}
